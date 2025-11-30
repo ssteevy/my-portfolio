dfa1f212b34c851a3b1301861ae3b8bcc6961902
ssteevy <steve.duvers@epitech.eu>
2025-11-30 14:40:39 +0100

    portfolio: add Header and NavBar

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-[#111827] min-h-screen">
+    <NavBar />
+    <HeroSection />
+  </div>
+</template>
+
+<script setup>
+import { defineAsyncComponent } from 'vue';
+
+const NavBar = defineAsyncComponent(() => import('@/components/NavBar.vue'));
+const HeroSection = defineAsyncComponent(() => import('@/components/HeroSection.vue'));
+//import HeroSection from '@/components/HeroSection.vue'
+</script>
